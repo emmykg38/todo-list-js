5b75c4015631e8867ce4933a6637847db592d87d
millylane003 <emilykgerst19@gmail.com>
2021-09-03 09:26:17 -0500

    working through displaytasks

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ let taskList = [];
 
 //starting the program, will eventually add everything to runProgram() function
 
-
-
 let userResponse = input.prompt(instructionPrompt);
 
 
@@ -25,16 +23,32 @@ let userResponse = input.prompt(instructionPrompt);
 
 function addTask(task) {
     taskList.push(task);
+    displayTasks();
 }
 
 
 function displayTasks() {
-    //might need array as parameter? need to think about it
-    //prints out all tasks with corresponding numbers (index + 1)
+    let displayedList;
+    let num;
+    let listItem;
+    for (let i = 0; i < taskList.length; i++) {
+        num = i + 1;
+    }
+    for (let j = 0; j < taskList.length; j++) {
+        listItem = num + ") " + taskList[j];
+    }
+    console.log(displayedList += listItem);
+
 }
 
 
-function deleteTask(task) {
+function deleteTask() {
+    displayTasks(taskList);
+    taskToDelete = Number(input.question("Please enter the number of the list-item you'd like to delete."));
+    indexToDelete = taskToDelete - 1;
+    taskList.splice(indexToDelete, 1);
+
+
     //accepts only positive integers, from 1 to 0 + taskList.length()
     //removes task at index - 1 of the number user inputs
     //removes selected task from the list
@@ -45,8 +59,8 @@ function deleteTask(task) {
 //requires validation: 
     //--must be 1, 2, 3, or 5
 function userResponseNumber() {
-    responseNumber = input.question(instructionPrompt);
-    while (responseNumber < 0 || responseNumber > 5 || responseNumber == 4) {
+    responseNumber = Number(input.question(instructionPrompt));
+    while (responseNumber < 0 || responseNumber > 5 || responseNumber === 4) {
         responseNumber = input.question("Oops! Invalid choice. Please select 1, 2, 3, or 5. Enter 0 to quit.");
     }
     return responseNumber;
@@ -57,9 +71,12 @@ function sortNumber(number) {
     if (number == 1) {
         userTask = input.question("Enter your task item here: ");
         addTask(userTask);
+    } else if (number === 2) {
+        deleteTask();
     } else {
-        return "nothing to see here";
+        return "no";
     }
+
 }
 
 
@@ -69,14 +86,22 @@ function welcomeToProgram() {
 }
 
 
+
+
 function runProgram() {
-    welcomeToProgram();
-    let number = userResponseNumber();
-    while((Number(number !== 0))) {
+}
+
+
+
+welcomeToProgram();
+let number = Number(userResponseNumber());
+while (number !== 0) {
+    sortNumber(number);
+    if (number === 0) {
+        break;
+    } else {
+        number = Number(userResponseNumber());
         sortNumber(number);
     }
 
 }
-
-
-runProgram();
